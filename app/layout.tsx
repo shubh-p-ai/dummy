b0d1399975b4fd6da47bbc8e3baca7726832ed2c
shubh-p-ai <chokerhere@gmail.com>
2025-11-27 13:41:56 +0530

    Update layout.tsx - changed title

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "MyAI3",
-  description: "MyAI3",
+  title: "LitLens",
+  description: "LitLens",
 };
 
 export default function RootLayout({
